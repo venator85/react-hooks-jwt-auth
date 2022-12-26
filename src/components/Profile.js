8d4cfd6c03edd58aa577f9a36209f61aca627a39
Alessio Bianchi <venator85@gmail.com>
2022-12-26 19:24:01 +0100

    migrated to react 18, update libs, clean

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,8 +12,7 @@ const Profile = () => {
         </h3>
       </header>
       <p>
-        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+        <strong>Token:</strong> {currentUser.accessToken}
       </p>
       <p>
         <strong>Id:</strong> {currentUser.id}
